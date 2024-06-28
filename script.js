e7bbf8567e47a6c0925d3f369b808902ed264208
Roberto Nares <roberto.nares@gmail.com>
2024-06-27 23:49:37 -0700

    Add functionality to create grid with any dimension

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+const grid = document.querySelector("#grid");
+
+function createGrid(size) {
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement("div");
+        row.classList.toggle("row");
+
+        for (let j = 0; j < size; j++) {
+            const square = document.createElement("div");
+            row.appendChild(square);
+        }
+
+        grid.appendChild(row);
+    }
+}
+
+createGrid(16);
