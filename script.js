8cd6d5d8a17e17600ce0006528b3f1d6f6ea0c42
Roberto Nares <roberto.nares@gmail.com>
2024-06-28 11:47:21 -0700

    Add hover effect that changes grid cell color

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const grid = document.querySelector("#grid");
 
+function changeSquareColor(event) {
+    event.target.classList.add("color");
+}
+
 function createGrid(size) {
     for (let i = 0; i < size; i++) {
         const row = document.createElement("div");
-        row.classList.toggle("row");
+        row.classList.add("row");
 
         for (let j = 0; j < size; j++) {
             const square = document.createElement("div");
+            square.addEventListener("mouseenter", changeSquareColor);
             row.appendChild(square);
         }
 
